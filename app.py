50868ce8252b69227218254db5d28e03be574ebd
CodeV0rtex <codevort3x0@gmail.com>
2026-02-21 16:27:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -826,4 +826,4 @@ def forbidden(e):
 
 if __name__ == '__main__':
     init_db()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
